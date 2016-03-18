cbb15ab36694931d18d454e0774e36340daa6bd0
Alex Parmentier <a.g.parmentier@gmail.com>
2016-03-18 14:08:31 -0400

    New: Setup for shared 2/3 distribution.
    Dir structure and setup files modified to allow installation of the correct
    version of the module.

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 # bootstrap easy_install
 from setuptools import setup, find_packages
+import sys
+
+#support for python2 and python3
+if sys.version_info[0] == 2:
+    base_dir = 'python2'
+elif sys.version_info[0] == 3:
+    base_dir = 'python3'
 
 setup(
     name = 'solrpy3',
-    version = '1',
-    author = 'Alex Parmentier',
-    author_email = 'a.g.parmentier@gmail.com',
-    url = 'https://github.com/lexpar/solrpy/tree/python3',
-    license = 'MIT',
+    version = '0.98',
+    author = 'Ed Summers',
+    author_email = 'ehs@pobox.com',
+    url = 'https://github.com/edsu/solrpy',
+    license = 'http://opensource.org/licenses/apache2.0.php',
     packages=['solr'],
-    install_requires = [],
-    description = 'Fork of solrpy 0.9.7 implementing python3 support.',
+    package_dir={
+        'solr' : base_dir + '/solr',
+    },
+    description = 'Client for the Solr search service',
     tests_require = ["nose>=0.10.1"],
     test_suite = 'nose.collector',
 
@@ -18,8 +27,7 @@ setup(
     [
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3 :: Only'
-
+        'Programming Language :: Python'
     ],
 
 )
